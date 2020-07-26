c1a5366c1d8434c57b5a9277f6bc38a9754f2293
UBS Developer <ubs.developer@MacBook-Pro-3.domain.name>
2020-07-26 15:35:58 +0530

    MealsApp v1 added

---
+++ MealsApp_flutter/MealsApp/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:MealsApp/screens/meal_detals_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/categories_screen.dart';
+import 'screens/category_meals_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DailyMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                  fontFamily: 'RobotoCondensed',
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+      ),
+      home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+        '/meal-details': (ctx) => MealDetailsScreen()
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoryMealsScreen());
+      },
+    );
+  }
+}
---
+++ MealsApp_flutter/MealsApp/lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:MealsApp/dummy_data.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Daily Meals"),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                id: catData.id, title: catData.title, color: catData.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
+    );
+  }
+}
